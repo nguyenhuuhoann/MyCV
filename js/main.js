6aad6118f7c27d858082dd97f60b99a0dfda6c54
NguyenHuuHoan <nguyenhuuhoann99@gmail.com>
2021-05-14 15:06:31 +0700

    add filter portfolio items

--- js/main.js
+++ js/main.js
@@ -31,3 +31,44 @@ const copyright = document.getElementById("year");
 const yearNow =new Date().getFullYear();
 copyright.innerHTML = `&copy; ${yearNow} Nguyen Huu Hoan`;
 })();
+
+
+// portfolio filter and popup
+(()=>{
+    const filterContainer = document.querySelector(".portfolio-filter"),
+    portfolioItemsContainer = document.querySelector(".portfolio-items"),
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    popup = document.querySelector(".portfolio-popup"),
+    prevBtn = popup.querySelector(".pp-prev"),
+    nextBtn = popup.querySelector(".pp-next"),
+    closeBtn = popup.querySelector(".pp-close"),
+    projectDetailsContainer = popup.querySelector(".pp-details"),
+    projectDetailsBtn =popup.querySelector(".pp-project-details-btn");
+
+    let itemIndex, slideIndex, screenshots;
+
+    // filter portfolio items 
+
+    filterContainer.addEventListener("click",(event)=>{
+        if(event.target.classList.contains("filter-item") && !event.target.classList.contains("active")){
+            // deactivate existing active 'filter item' 
+            filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
+            // active new 'filter item'
+            event.target.classList.add("active","outer-shadow");
+            const target = event.target.getAttribute("data-target");
+            portfolioItems.forEach((item)=>{
+                if(target===item.getAttribute("data-category") || target === 'all'){
+                    item.classList.remove("hide");
+                    item.classList.add("show");
+
+                }else{
+                    item.classList.remove("show");
+                    item.classList.add("hide");
+                }
+            })
+        }
+    })
+})();
+
+
+ 
