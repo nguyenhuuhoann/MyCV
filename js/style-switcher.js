7b0a54945d78e5c6332cdb5ffe50d05bd5aee714
nguyenhuuhoann <nguyenhuuhoann99@gmail.com>
2021-07-11 21:38:44 +0700

    a

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -18,7 +18,7 @@ window.addEventListener("scroll", () => {
 
 const alternateStyles = document.querySelectorAll(".alternate-style");
 
-function ss(color) {
+function ssaaa(color) {
     localStorage.setItem("color", color);
     changeColor();
 
